d16e0570b66908f423199689639fa80efc47fde9
vortreex <krzystbg11@gmail.com>
2018-09-04 23:20:51 +0200

    minor refactorin (added countPropabilitest to the class)

--- tANS.py
+++ tANS.py
@@ -1,13 +1,19 @@
 import sys
-import tanslib
+from tanslib import *
 
 print('Compressing file: ' + str(sys.argv[1]) + ' with tANS method')
 
 path = str(sys.argv[1])
-data = tanslib.openFileAsByteArray(path)
-probabilitiesTable = tanslib.countProbabilities(data)
-tansTable = tanslib.ReferenceTable().createReferenceTable(probabilitiesTable)
-compressedData = tanslib.compress(data, tansTable)
-compressedData = tanslib.convertBitsToByteArray(compressedData)
-compressedData = tanslib.addHeader(compressedData, probabilitiesTable)
-tanslib.saveByteArrayAsFile(compressedData, sys.argv[1])
+data = openFileAsByteArray(path)
+CompressionEngine = ReferenceTable()
+probabilitiesTable = CompressionEngine.countProbabilities(data)
+tansTable = CompressionEngine.createReferenceTable()
+compressedData = compress(data, tansTable)
+cprdata1 = compressedData
+compressedData, numOfBits = convertBitsToByteArray(compressedData)
+cprdata2 = compressedData
+compressedData = addHeader(compressedData, probabilitiesTable, numOfBits)
+saveByteArrayAsFile(compressedData, sys.argv[1])
+probalTab , comprData2, lastBitsnumb = removeHeader(compressedData)
+comprData1 = convertByteArrayToBits(comprData2, lastBitsnumb)
+
--- tanslib.py
+++ tanslib.py
@@ -1,26 +1,38 @@
 import math
 import pickle
-
+headerLenght = 2834
 
 class ReferenceTable(object):
     def __init__(self):
         self.referenceTable = []
         self.columnsLength = []
-        self.probabilitesHeader = []
+        self.Probabilities = []
+        self.probabilitiesHeader = []
         self.maxVal = 255 + 2 ** 8
 
-    def createReferenceTable(self, probabilites):
-        valuesUsed = []
-        self.probabilitesHeader = probabilites[0]
+    def countProbabilities(self, Data):
+        probabilitiesTable = [0] * 256
+        for i in Data:
+            probabilitiesTable[i] += 1
+        for i in range(0, 256):
+            probabilitiesTable[i] /= len(Data)
 
-        for i in range(0, len(probabilites[0])):
-            self.columnsLength.append(math.floor(probabilites[1][i] * self.maxVal))
+        self.Probabilities = [[a for a in range(0, 256)], probabilitiesTable]
+
+        return bubbleSort(self.Probabilities)
+
+    def createReferenceTable(self):
+        valuesUsed = []
+        self.probabilitiesHeader = self.Probabilities[0]
+
+        for i in range(0, len(self.probabilitiesHeader)):
+            self.columnsLength.append(math.floor(self.Probabilities[1][i] * self.maxVal))
         self.columnsLength[0] += 1
 
         for rowNo in range(1, round(max(self.columnsLength) + 1)):
             row = []
-            for j in range(0, len(probabilites[0])):
-                value = round(rowNo / probabilites[1][j])
+            for j in range(0, len(self.Probabilities[0])):
+                value = round(rowNo / self.Probabilities[1][j])
                 value = incrementValueTillNotUsed(value, valuesUsed)
                 if rowNo <= self.columnsLength[j]:
                     row.append(value)
@@ -40,32 +52,28 @@ def openFileAsByteArray(filePath):
     return byteMap
 
 
-def convertBitsToByteArray(bits):
-    byteArray = []
-    for i in range(0, int(math.ceil(len(bits) / 8))):
-        byteArray.append(int(bits[i * 8:(i + 1) * 8], 2))
-    return byteArray
-
-
 def saveByteArrayAsFile(compressedData, fileName):
-    newName = str(fileName).split('/')[-1] + '.tans'
-    f = open(newName, 'wb')
+    fileName = str(fileName).split('/')[-1] + '.tans'
+    f = open(fileName, 'wb')
     f.write(compressedData)
     f.close()
     print("File's size  after compression is: " + str(len(compressedData)) + ' bytes')
 
 
-def countProbabilities(Data):
-    probabilitesTable = [0]*256
-    for i in Data:
-        probabilitesTable[i] += 1
-    for i in range(0, 256):
-        probabilitesTable[i] /= len(Data)
+def convertBitsToByteArray(bits):
+    byteArray = []
+    for i in range(0, int(math.ceil(len(bits) / 8))):
+        byteArray.append(int(bits[i * 8:(i + 1) * 8], 2))
 
-    probabilitesTable = [[a for a in range(0, 256)], probabilitesTable]
-    probabilitesTable = bubbleSort(probabilitesTable)
+    return byteArray, len(bits) % 8 # druga wartosc zwracana to liczba bitow w ostatnim bajcie, bo nie zawsze to jest8 i sie kod pieprzy
 
-    return probabilitesTable
+
+def convertByteArrayToBits(byteArray, numOfLastBits):
+    bits = ''
+    for byte in byteArray[:-1]:
+        bits += str(bin(byte))[2:].zfill(8)
+    bits += str(bin(byteArray[-1]))[2:].zfill(numOfLastBits)
+    return bits
 
 
 def compress(data, tansTab):
@@ -73,7 +81,7 @@ def compress(data, tansTab):
     initialState = tansTab.referenceTable[-1][0] ### maxVal
 
     for symbol in data:
-        targetRow = tansTab.columnsLength[tansTab.probabilitesHeader.index(symbol)]
+        targetRow = tansTab.columnsLength[tansTab.probabilitiesHeader.index(symbol)]
         state = [initialState, symbol]
         while state[0] > targetRow:
             if state[0] % 2 == 0:
@@ -82,21 +90,26 @@ def compress(data, tansTab):
                 compressedData += '1'
             state[0] >>= 1
 
-        initialState = tansTab.referenceTable[state[0]-1][tansTab.probabilitesHeader.index(symbol)]
+        initialState = tansTab.referenceTable[state[0]-1][tansTab.probabilitiesHeader.index(symbol)]
     return compressedData
 
 
-def addHeader(data, probalTab):
+def addHeader(data, probalTab, numOfLastBits):
     header = list(pickle.dumps(probalTab))
-    separator = [255]*4
-    return bytes(header + separator + data)
-
-def removeHeader(data):
-    pass
+    return bytes(header + [numOfLastBits] + data)
 
 
-def decompress(comprData,tansTab):
-    pass
+def removeHeader(compressedData):
+    compressedData=list(compressedData)
+    header = compressedData[:headerLenght]
+    data = compressedData[headerLenght+1:]
+    probabilitestTable = pickle.loads(bytes(header))
+    return probabilitestTable, data, compressedData[headerLenght]
+
+def decompress(comprData, tansTab):
+    decompressedData = []
+
+    return decompressedData
 
 
 def incrementValueTillNotUsed(val, usedValues):
